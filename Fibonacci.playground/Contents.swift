2cf70b6eb740a7b81deb3721b7841ea86c79c492
Felix Schmidt <felix.schmidt@metafinanz.de>
2017-07-07 11:58:34 +0200

    iterative solution

--- Fibonacci.playground/Contents.swift
+++ Fibonacci.playground/Contents.swift
@@ -17,3 +17,17 @@ func fibonacciNumbersRecursive(for count: Int, first: Int, second: Int) -> [Int]
     return [first] + fibonacciNumbersRecursive(for: count-1, first: second, second: (second+first))
 }
 _ = fibonacciNumbersRecursive(for: 10, first: 0, second: 1)
+
+
+/**
+ Iterativ Solution
+ **/
+func fibonacciNumbersIterative(for count: Int) -> [Int] {
+    var numbers = [0, 1]
+    for _ in 0...count-3 {
+        let nextNumber = numbers[numbers.count - 2] + numbers.last!
+        numbers.append(nextNumber)
+    }
+    return numbers
+}
+_ = fibonacciNumbersIterative(for: 10)
